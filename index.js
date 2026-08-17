0a4f5cae97044c7302c3fd1608f66756210eaee0
ebccoats <coats.emma@gmail.com>
2026-08-17 15:24:10 -0400

    get scroll background working

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
 // description: This example demonstrates how to use a Container to group and manipulate multiple sprites
 import { Application, Assets, Container, Sprite } from 'pixi.js';
 
-(async () => {
+
+  const container = new Container();
+
+const screenWidth = 256
+const screenHeight = 240
+
+const scaleFactor = 3
+
+let positionX
+let positionY
+
+
+async function init() {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: '#1099bb', resizeTo: window });
+  await app.init({ 
+      background: '#1099bb', 
+      width: screenWidth,
+      height: screenHeight,
+      resolution: scaleFactor,
+  });
+    app.canvas.style.imageRendering = "pixelated"
+
 
   // Append the application canvas to the document body
   document.body.appendChild(app.canvas);
 
   // Create and add a container to the stage
-  const container = new Container();
 
   app.stage.addChild(container);
 
   // Load the bunny texture
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+  const texture = await Assets.load('./assets/PaperboyMapBG.png');
+    texture.source.scaleMode = "nearest"
 
   // Create a 5x5 grid of bunnies in the container
-  for (let i = 0; i < 25; i++) {
-    const bunny = new Sprite(texture);
-
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
-  }
-
+    const background = new Sprite(texture)
+    container.addChild(background)
+    
   // Move the container to the center
-  container.x = app.screen.width / 2;
-  container.y = app.screen.height / 2;
+  container.x = 0
+  container.y = -(texture.height - screenHeight)
+
+    positionX = container.x
+    positionY = container.y
 
   // Center the bunny sprites in local container coordinates
-  container.pivot.x = container.width / 2;
-  container.pivot.y = container.height / 2;
 
   // Listen for animate update
-  app.ticker.add((time) => {
-    // Continuously rotate the container!
-    // * use delta to create frame-independent transform *
-    container.rotation -= 0.01 * time.deltaTime;
-  });
-})();
+    app.ticker.add(update);
 
+}
+
+init()
+
+const containerSpeedY = 0.5
+const containerSpeedX = -0.5
+
+let accumulator = 0
+let frequency = 1000/32 // 20 updates per second (1000 ms)
+
+const update = (time) => {
+    accumulator += time.deltaMS
+
+    if (accumulator >= frequency) {
+        positionX += (containerSpeedX * time.deltaTime)
+        positionY += (containerSpeedY * time.deltaTime)
+
+        container.x = Math.floor(positionX)
+        container.y = Math.floor(positionY)
+
+        accumulator -= frequency
+
+    }
+
+}
